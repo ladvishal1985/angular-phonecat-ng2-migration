aea05e9652d8f95288e54769e78738c7c0d29f16
Vishal lad <ladvishal1985@gmail.com>
2016-01-11 16:12:39 +0530

    Added the phone detail module

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -5,18 +5,24 @@
 var phonecatApp = angular.module('phonecatApp', [
     'ngRoute',
     'phonecat.core',
-    'phonecat.list'
+    'phonecat.list',
+    'phonecat.detail'
 ]);
 
 phonecatApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/phones', {
-        templateUrl: 'js/phone_list/phone_list.html',
-        controller: 'PhoneListCtrl',
-        controllerAs: 'vm'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
+        when('/phones', {
+            templateUrl: 'js/phone_list/phone_list.html',
+            controller: 'PhoneListCtrl',
+            controllerAs: 'vm'
+        }).
+        when('/phones/:phoneId', {
+            templateUrl: 'js/phone_detail/phone_detail.html',
+            controller: 'PhoneDetailCtrl',
+            controllerAs: 'vm'
+        }).
+        otherwise({
+            redirectTo: '/phones'
+        });
   }]);
