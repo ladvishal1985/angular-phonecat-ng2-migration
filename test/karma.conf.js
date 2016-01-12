e43764c00171e6e72ea9532eeb1f47b1f6c113f2
Vishal lad <ladvishal1985@gmail.com>
2016-01-12 17:52:35 +0530

    Step-3: Added Angular 2.0 dependencies

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -10,6 +10,12 @@ module.exports = function(config){
       'app/bower_components/angular-animate/angular-animate.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'node_modules/systemjs/dist/system.src.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/es6-shim/es6-shim.js',
+      'node_modules/es6-promise/dist/es6-promise.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/angular2/bundles/angular2.dev.js',
+      'node_modules/angular2/bundles/upgrade.dev.js',
       'test/karma_test_shim.js',
       {pattern: 'app/js/**/*.js', included: false, watched: true},
       {pattern: 'test/unit/**/*.js', included: false, watched: true}
