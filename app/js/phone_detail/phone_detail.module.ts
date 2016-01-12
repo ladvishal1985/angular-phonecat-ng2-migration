666ec20c7ba3218d2da856130d8b6c04ed32b0ce
Vishal lad <ladvishal1985@gmail.com>
2016-01-12 17:53:19 +0530

    Step-3: Removed the animation. Address later

--- app/js/phone_detail/phone_detail.module.ts
+++ app/js/phone_detail/phone_detail.module.ts
@@ -1,6 +1,4 @@
 import PhoneDetailCtrl from './phone_detail.controller';
-import phoneAnimation from './phone_detail.animations';
 
-export default angular.module('phonecat.detail', ['ngRoute','phonecat.core'])
-	.controller('PhoneDetailCtrl', PhoneDetailCtrl)
-	.animation('.phone', phoneAnimation);
+export default angular.module('phonecat.detail', ['ngRoute', 'phonecat.core'])
+	.controller('PhoneDetailCtrl', PhoneDetailCtrl);
